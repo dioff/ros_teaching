74ff5d6918dfa834e93f5090e4a82acb6e8c91cd
dioff <2274902520@qq.com>
2024-01-27 01:17:26 +0800

    第一次一股脑提交

---
+++ src/test/src/helloworld.cpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"
+
+int main(int argc, char *argv[])
+{
+    // 初始化ROS节点
+    ros::init(argc, argv, "hello");
+    // 创建ROS节点句柄
+    ros::NodeHandle nh;
+    // 在控制台输出hello world
+    ROS_INFO("hello world");
+    return 0;
+}
